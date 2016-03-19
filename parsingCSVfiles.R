98fb8be17401575b3eeda5cdfa4d5d0907ad5235
arg2211 <arg2211@columbia.edu>
2016-03-18 22:53:26 -0400

    make and clean corpus

--- parsingCSVfiles.R
+++ parsingCSVfiles.R
@@ -6,13 +6,53 @@ options(stringsAsFactors = FALSE)
 
 library(tm)
 
-# load .csv file containing text and metadata as corpus
+# load .csv file containing text and metadata
 # (created with my edited version of Neal Caren's Python script split_ln.py)
-nyt <- read.csv('nyt3.7b.csv', header = TRUE)
-nyttext <- VCorpus
+nyt <- read.csv('nyt3.7b.csv', header = TRUE, stringsAsFactors = FALSE)
+
+# create a corpus from text and metadata
 getReaders()
 nytcorpus <- VCorpus(DataframeSource(nyt))
-class(nytcorpus)
-inspect(nytcorpus)
-inspect(nytcorpus[2])
+class(nytcorpus) #just making sure it's a corpus
+inspect(nytcorpus) #gives info about all "documents" in corpus
+inspect(nytcorpus[2]) #gives info about only the 2nd "document" in corpus
 summary(nytcorpus)
+
+# create a corpus with only text in it
+onlytext <- paste(nyt$TEXT, collapse = " ", stringsAsFactors = FALSE)
+class(onlytext)
+summary(onlytext)
+onlytextvs <- VectorSource(onlytext)
+onlytextcorpus <- Corpus(onlytextvs)
+class(onlytextcorpus)
+
+# clean onlytextcorpus
+toSpace <- content_transformer(function(x, pattern) {return (gsub(pattern, " ", x))})
+
+
+getTransformations()
+otc <- tm_map(onlytextcorpus, content_transformer(tolower)) #need to use content_transformer with tolower because of bug in newer version of tm package
+otc <- tm_map(otc, removePunctuation)
+otc <- tm_map(otc, toSpace, "-")
+otc <- tm_map(otc, toSpace, ":")
+otc <- tm_map(otc, stripWhitespace)
+otc <- tm_map(otc, removeWords, stopwords("english"))
+#otc <- tm_map(otc, removeWords, c("x")) #insert words that you want to remove from corpus where "x" is
+inspect(otc)
+class(otc)
+
+# make a document term matrix (dtm)
+dtm <- DocumentTermMatrix(otc)
+dtm2 <- as.matrix(dtm)
+dtm
+
+# find most frequent terms
+freq <- colSums(dtm2)
+str(freq)
+freq <- sort(freq, decreasing = TRUE)
+head(freq)
+
+# create a wordcloud
+library(wordcloud)
+words <- names(freq)
+wordcloud(words[1:100], freq[1:100])
